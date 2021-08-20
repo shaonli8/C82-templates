6f9609cf79f9997a80a8e794589ba3eec712c9ff
shaonli8 <72174470+shaonli8@users.noreply.github.com>
2021-08-20 09:36:32 +0530

    Create RecieverDetailsScreen-stylesheet.js

---
+++ RecieverDetailsScreen-stylesheet.js
@@ -0,0 +1,27 @@
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  upperContainer: {
+    flex: 0.3
+  },
+  middleContainer: {
+    flex: 0.3
+  },
+  cardTitle: {
+    fontSize: 20
+  },
+  lowerContainer: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  button: {
+    width: 200,
+    height: 50,
+    borderRadius: 10
+  },
+  buttonText: {
+    fontWeight: "300"
+  }
+});
